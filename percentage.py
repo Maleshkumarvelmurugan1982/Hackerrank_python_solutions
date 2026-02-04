04d15450cf79c5e445f880dc18e22c45abdab753
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-02-04 08:09:50 +0530

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,10 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    line = input().split()
+    name = line[0]
+    marks = list(map(float, line[1:]))
+    student_marks[name] = marks
+query_name = input()
+average_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
+print(f"{average_marks:.2f}")
